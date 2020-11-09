51dbed9ce5806d7144a31e433edf350220ac6cdf
parkpoomd <parkpoom.d@gmail.com>
2020-11-09 13:01:43 +0700

    refac code PropertyCard.vue

--- src/components/PropertyCard.vue
+++ src/components/PropertyCard.vue
@@ -26,28 +26,23 @@
           {{ property.title }}
         </h4>
         <div class="mt-1">
-          <span class="text-gray-900">{{ formattedPrice }}</span
-          ><span class="ml-1 text-sm text-gray-600">/wk</span>
+          <span class="text-gray-900">{{ formattedPrice }}</span>
+          <span class="ml-1 text-sm text-gray-600">/wk</span>
         </div>
         <div class="mt-2 flex items-center text-sm text-gray-600">
           <svg
             v-for="i in 5"
             :key="i"
-            class="w-4 h-4"
+            class="h-4 w-4 fill-current"
             :class="property.rating >= i ? 'text-teal-500' : 'text-gray-400'"
-            fill="currentColor"
-            stroke="currentColor"
             viewBox="0 0 24 24"
             xmlns="http://www.w3.org/2000/svg"
           >
             <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"
-            ></path>
+              d="M8.128 19.825a1.586 1.586 0 0 1-1.643-.117 1.543 1.543 0 0 1-.53-.662 1.515 1.515 0 0 1-.096-.837l.736-4.247-3.13-3a1.514 1.514 0 0 1-.39-1.569c.09-.271.254-.513.475-.698.22-.185.49-.306.776-.35L8.66 7.73l1.925-3.862c.128-.26.328-.48.577-.633a1.584 1.584 0 0 1 1.662 0c.25.153.45.373.577.633l1.925 3.847 4.334.615c.29.042.562.162.785.348.224.186.39.43.48.704a1.514 1.514 0 0 1-.404 1.58l-3.13 3 .736 4.247c.047.282.014.572-.096.837-.111.265-.294.494-.53.662a1.582 1.582 0 0 1-1.643.117l-3.865-2-3.865 2z"
+            />
           </svg>
-          <span class="ml-2"> {{ property.reviewCount }} reviews </span>
+          <span class="ml-2">{{ property.reviewCount }} reviews</span>
         </div>
       </div>
     </div>
